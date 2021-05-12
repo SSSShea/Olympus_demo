08afb6864c12154a904f93fbac0688deb3f4ae40
SSSShea <caixuyi20b@ict.ac.cn>
2021-05-12 17:57:25 +0800

    modify .gitignore

--- installrootfs.sh
+++ installrootfs.sh
@@ -2,6 +2,8 @@
 
 echo "install rootfs image from: https://www.dropbox.com/s/qa9wgd2jd8j78a6/olympus_rootfs.tar.gz?dl=0"
 
+mkdir rootfs
+
 curl -L -o ./rootfs/olympus_rootfs.tar.gz https://www.dropbox.com/s/qa9wgd2jd8j78a6/olympus_rootfs.tar.gz?dl=0
 
 echo "complete!"
